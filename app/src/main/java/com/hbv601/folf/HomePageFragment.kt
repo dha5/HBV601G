139bec39b233b5392247fe09b8d62d444ba39cb4
YellowWall <89491565+YellowWall@users.noreply.github.com>
2024-02-28 19:07:34 +0000

    breytti Id vísun í rétt id

--- app/src/main/java/com/hbv601/folf/HomePageFragment.kt
+++ app/src/main/java/com/hbv601/folf/HomePageFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.hbv601.folf.R
 
 
 class HomePageFragment : Fragment() {
@@ -16,14 +15,16 @@ class HomePageFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home_page, container, false)
 
-        val textView = view.findViewById<TextView>(R.id.textView)
+        val textView = view.findViewById<TextView>(R.id.textMessage)
 
         val userName = "John" // þarf að skipta út fyrir notendanafn
         val numInvitations = 5 // þarf að skipta út fyrir fjölda invites sem notandi hefur fengið
         val numOpenMatches = 10 // þarf að skipta út fyrir fjölda opnra leikja
 
         val welcomeMessage = getString(R.string.home_page_welcome_message, userName, numInvitations, numOpenMatches)
-        textView.text = welcomeMessage
+        if(textView != null) {
+            textView.text = welcomeMessage
+        }
 
         return view
     }
