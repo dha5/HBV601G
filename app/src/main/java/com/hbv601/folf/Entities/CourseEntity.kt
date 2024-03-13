2a7bb15ce743897221923d7f1d1913fe3615ab3d
Ingimargudna <92264956+Ingimargudna@users.noreply.github.com>
2024-03-13 20:39:33 +0000

    smá bras í course entity

--- app/src/main/java/com/hbv601/folf/Entities/CourseEntity.kt
+++ app/src/main/java/com/hbv601/folf/Entities/CourseEntity.kt
@@ -1,4 +1,9 @@
 package com.hbv601.folf.Entities
+import android.location.Location
 
-class CourseEntity {
+
+class CourseEntity(var courseTitle:String? = null , var coursePar:String? = null) {
+
+
+
 }
