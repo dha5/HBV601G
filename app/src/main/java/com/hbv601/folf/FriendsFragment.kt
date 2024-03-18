96d08d611b546310ff99d9f9cb6ab59161f0652c
YellowWall <89491565+YellowWall@users.noreply.github.com>
2024-03-18 16:35:48 +0000

    Friends fragment and binding created
    enginn sérstakur kóði kominn

---
+++ app/src/main/java/com/hbv601/folf/FriendsFragment.kt
@@ -0,0 +1,29 @@
+package com.hbv601.folf
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hbv601.folf.databinding.FragmentFriendsBinding
+
+class FriendsFragment: Fragment() {
+    private var _binding: FragmentFriendsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    fun fetchUsers(){
+
+    }
+    fun fetchFriends(){}
+}
