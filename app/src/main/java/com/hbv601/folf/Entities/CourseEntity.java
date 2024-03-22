006c15236503fd281b379c96cdffdaee160a6a72
Ingimargudna <92264956+Ingimargudna@users.noreply.github.com>
2024-03-22 22:01:46 +0000

    courseEntity fullkomnað

--- app/src/main/java/com/hbv601/folf/Entities/CourseEntity.java
+++ app/src/main/java/com/hbv601/folf/Entities/CourseEntity.java
@@ -1,6 +1,5 @@
 package com.hbv601.folf.Entities;
 import static androidx.core.content.ContextCompat.getSystemService;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -9,28 +8,43 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-
+import androidx.lifecycle.LifecycleOwnerKt;
+import androidx.lifecycle.ViewModel;
+import com.hbv601.folf.network.FolfApi;
+import com.hbv601.folf.network.FolfApiService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import retrofit2.Response;
 
 
-public class CourseEntity {
+public class CourseEntity extends ViewModel {
 
     private String courceName;
+
+    private String description;
+
+    private int id;
     private List<Integer> coursePars;
     private Location courseLocation;
 
 
 
-    public CourseEntity(String name, List<Integer> par, Location coordinates){
+    public CourseEntity(String name, List<Integer> par, String coordinates, String desc, int id){
         this.courceName = name;
         this.coursePars = par;
-        this.courseLocation = coordinates;
+        this.id=id;
+        this.description = desc;
+        String[] parts = coordinates.split(";");
+        double lat = Double.parseDouble(parts[0].trim());
+        double lon = Double.parseDouble(parts[1].trim());
+        Location loc = new Location(name);
+        loc.setLatitude(lat);
+        loc.setLongitude(lon);
+        this.courseLocation = loc;
     }
 
     public static CourseEntity generateDummy(){
@@ -38,7 +52,7 @@ public class CourseEntity {
         loc.setLatitude(64.1397116);
         loc.setLongitude(-21.9478740); //hnitin á háskóla íslands
         int[] pars = {3,3,4,4,3};
-        CourseEntity dummyCourse = new CourseEntity("dummy", Arrays.stream(pars).boxed().collect(Collectors.toList()), loc);
+        CourseEntity dummyCourse = new CourseEntity("dummy", Arrays.stream(pars).boxed().collect(Collectors.toList()), "64.1397116;-21.9478740","test",0);
         return dummyCourse;
     }
 
@@ -104,6 +118,23 @@ public class CourseEntity {
         }
         Log.d("Kemst ekki í fall","cant make it");
         return 0;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public List<Integer> getCoursePars() {
